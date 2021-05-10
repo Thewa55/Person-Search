26982765f527fd12527295686d39189820435898
Kenny Lam <kinwai.lam730@gmail.com>
2021-05-09 23:25:37 -0400

    Added more information fields

--- src/App.js
+++ src/App.js
@@ -14,15 +14,33 @@ class App extends Component{
   componentDidMount(){
     fetch('https://jsonplaceholder.typicode.com/users')
     .then(res => res.json())
-    .then(users => this.setState({employees: users}))
+    .then(users => {
+      this.setState({employees: users})
+      console.log(users)
+    })
   }
 
   render(){
     return(
     <div className="App">
         {
-          this.state.employees.map(employee => 
-            <h1 key={employee.id}> {employee.name}</h1>
+          this.state.employees.map(employee =>
+            <div key={employee.id}> 
+              <h1>Name: {employee.name}</h1>
+              <div>
+                <h2>Address:</h2>
+                <p>{employee.address.street}</p>
+                <p>{employee.address.suite}</p>
+                <p>{employee.address.city}, {employee.address.zipcode}</p>
+              </div>
+              <div>
+                <h2>Company:</h2>
+                <p>{employee.company.name}</p>
+                <p>{employee.company.bs}</p>
+                <p>{employee.company.catchPhrase}, {employee.address.zipcode}</p>
+              </div>
+            </div>
+            
           )
         }
     </div>
