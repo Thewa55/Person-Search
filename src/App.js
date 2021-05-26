17026f8b072555356f3e62fab7ec1f04ef21293e
Kenny Lam <kinwai.lam730@gmail.com>
2021-05-25 21:53:52 -0400

    Added useRef to input fields and function to map inputs

--- src/App.js
+++ src/App.js
@@ -55,7 +55,9 @@ class App extends Component{
     this.setState({show: !this.state.show})
   }
 
-
+  newPerson = (person) => {
+    
+  }
 
   render(){
 
--- src/components/modal/modal.js
+++ src/components/modal/modal.js
@@ -1,9 +1,29 @@
-import React from "react";
+import React, {useRef} from "react";
 import './modal.css'
 
 export const Modal = (props) => {
 
+  const cellRef = useRef()
+  const emailRef= useRef()
+  const fullnameRef= useRef()
+  const addressRef = useRef()
   const onClose= (e) => {props.onClose()}
+  const newPerson = (e)=> { 
+    e.preventDefault()
+    let newPerson = {
+        cell: cellRef.current.value,
+        email: emailRef.current.value,
+        name: {
+            first: fullnameRef.current.value
+        },
+        location: {
+            street: {
+                name: addressRef.current.value
+            }
+        }
+    }
+    console.log(newPerson);
+  }
 
   return (
     <>
@@ -14,12 +34,12 @@ export const Modal = (props) => {
                 <div className='Modal'>
                     <h2>Modal Window</h2>
                     <form>
-                        <input type='text' placeholder='Name'></input>
-                        <input type='email' placeholder='E-mail'></input>
-                        <input type='telephone' placeholder='Cell'></input>
-                        <input type='text' placeholder='Address'></input>                    
+                        <input type='text' placeholder='Name' ref={fullnameRef}></input>
+                        <input type='email' placeholder='E-mail' ref={emailRef}></input>
+                        <input type='telephone' placeholder='Cell' ref={cellRef}></input>
+                        <input type='text' placeholder='Address'ref={addressRef}></input>                    
                         <div class="actions">
-                            <button type='submit'>Submit</button>
+                            <button type='submit' onClick={newPerson}>Submit</button>
                             <button onClick={onClose} class="toggle-button">Close Modal</button>
                         </div>
                     </form>
