2fe76adbdd6df17df2c8e2c633e182bf784e76c5
Kenny Lam <kinwai.lam730@gmail.com>
2021-06-01 14:53:55 -0400

    Finished validations on fields to ensure no blanks are allowed and added button disabling if a field is blank

--- src/components/modal/modal.js
+++ src/components/modal/modal.js
@@ -2,7 +2,8 @@ import React, {useRef, useEffect, useState} from "react";
 import './modal.css'
 
 export const Modal = (props) => {
-
+  
+  const [disabled, setDisabled] = useState(true)
   const cellRef = useRef()
   const emailRef= useRef()
   const fullnameRef= useRef()
@@ -11,62 +12,40 @@ export const Modal = (props) => {
   const cityRef = useRef()
   const zipRef= useRef()
   const idRef= useRef()
-  const [errorArr, setErrorArr] = useState([])
+  const [errorMessage, setErrorMessage] = useState ({
+    Cell: '',
+    Email: '',
+    Name: '',
+    Address: '',
+    City: '',
+    State: '',
+    Zip: '',
+    Id: ''})
 
-  let initError = {
-    cellError: '', 
-    emailError: '',
-    nameError: '',
-    addressError: '',
-    cityError: '',
-    stateError:'',
-    idError: ''}
-
-  useEffect(() => {console.log(errorArr)}, [errorArr])
+  useEffect(() => {console.log(errorMessage)}, [errorMessage])
   const onClose= (e) => {props.onClose()};
+
+  const inputValidation = (e) => {
+    console.log('Input Validation')
+    const fieldName = e.target.name
+    if(e.target.value === ''){
+        setErrorMessage(prevState => ({...prevState, [fieldName]: `${fieldName} is required`}))
+    }
+  }  
+  
+  const onChange = (e) => {
+    let fieldName = e.target.name
+    setErrorMessage(prevState => ({ ...prevState, [fieldName]: ''}))
+    if(fullnameRef.current.value !== '' && addressRef.current.value !== '' && cellRef.current.value !== '' && emailRef.current.value !== ''&& cellRef.current.value !== '' && stateRef.current.value !== ''&& idRef.current.value !== ''&& zipRef.current.value !== ''){
+        setDisabled(false)
+    } else {
+        setDisabled(true)
+    }
+
+  }
 
   const newPerson = (e)=> { 
     e.preventDefault()
-    let errorStore = []
-    setErrorArr(errorStore)
-    if(cellRef.current.value === ''){
-        errorStore.push('Cell Phone Number Required')
-        // setErrorArr([...errorArr, 'Cell Phone Number Required'])
-    }
-    if(emailRef.current.value === ''){
-        errorStore.push('Email required')
-        //setErrorArr([...errorArr, 'Email required'])
-    } 
-    if(fullnameRef.current.value === ''){
-        errorStore.push('Name required')
-        // errorMessage.fullNameError = 'Name required'
-        //setErrorArr([...errorArr, 'Name required'])
-    } 
-    if (addressRef.current.value === ''){
-        errorStore.push('Address required')
-        // errorMessage.addressError = 'Address required'
-        // setErrorArr([...errorArr, 'Address required'])
-    }
-    if (cityRef.current.value === ''){
-        errorStore.push('City required')
-        // errorMessage.cityError = 'City required'
-       // setErrorArr([...errorArr, 'City required'])
-    }
-    if (stateRef.current.value === ''){
-        errorStore.push('State required')
-        // errorMessage.stateError = 'State required'
-        //setErrorArr([...errorArr, 'State required'])
-    }
-    if (idRef.current.value === ''){
-        errorStore.push('ID required')
-        // errorMessage.idError = 'ID required'
-        // setErrorArr([...errorArr, 'ID required'])
-    };
-    if(errorStore.length > 0){
-        setErrorArr([...errorArr, errorStore])
-        // setErrorArr(errorArr.concat(errorStore))
-    }
-    if(!errorArr){
         let newPerson = {
             picture: { large: ''},
             cell: cellRef.current.value,
@@ -85,7 +64,7 @@ export const Modal = (props) => {
             }
         }
         props.createPerson(newPerson)
-    } 
+
   }
 
   return (
@@ -96,37 +75,45 @@ export const Modal = (props) => {
             ):(
                 <div className='Modal'>
                     <h2>Modal Window</h2>
-                    <div className='Errorsdiv'>
+                    {/* <div className='Errorsdiv'>
                         { errorArr.length > 0 ? (errorArr.map(error => ( <div className='Error'>{error}</div>))): (<div></div>) } 
-                    </div>
+                    </div> */}
                     <form>
                         <div>
-                            <input type='text' placeholder='Name' ref={fullnameRef}></input>
+                            <input type='text' name='Name' placeholder='Name' onBlur={inputValidation} ref={fullnameRef} onChange={onChange}></input> 
+                            {errorMessage.Name !== '' ? (<div className='Error'>{errorMessage.Name}</div>) : null }
                         </div>
                         <div>
-                            <input type='email' placeholder='E-mail' ref={emailRef}></input>
+                            <input type='email' name='Email' placeholder='E-mail' onBlur={inputValidation} ref={emailRef} onChange={onChange}></input>
+                            {errorMessage.Email !== '' ? (<div className='Error'>{errorMessage.Email}</div>) : null }
                         </div>
                         <div>
-                            <input type='telephone' placeholder='Cell' ref={cellRef}></input>
+                            <input type='telephone' name='Cell' placeholder='Cell' onBlur={inputValidation} ref={cellRef} onChange={onChange}></input>
+                            {errorMessage.Cell !== '' ? (<div className='Error'>{errorMessage.Cell}</div>) : null }
                         </div>
                         <div>
-                            <input type='text' placeholder='Address'ref={addressRef}></input>
+                            <input type='text' name='Address' placeholder='Address' onBlur={inputValidation} ref={addressRef} onChange={onChange}></input>
+                            {errorMessage.Address !== '' ? (<div className='Error'>{errorMessage.Address}</div>) : null }
                         </div>
                         <div>    
-                            <input type='text' placeholder='City' ref={cityRef}></input>
+                            <input type='text' name='City' placeholder='City' onBlur={inputValidation} ref={cityRef} onChange={onChange}></input>
+                            {errorMessage.City !== '' ? (<div className='Error'>{errorMessage.City}</div>) : null }
                         </div>
                         <div>
-                            <input type='text' placeholder='State'ref={stateRef}></input>
+                            <input type='text' name='State' placeholder='State' onBlur={inputValidation} ref={stateRef} onChange={onChange}></input>
+                            {errorMessage.State !== '' ? (<div className='Error'>{errorMessage.State}</div>) : null }
                         </div> 
                         <div>  
-                            <input type='text' placeholder='Zip Code'ref={zipRef}></input>
+                            <input type='text' name='Zip' placeholder='Zip Code' onBlur={inputValidation} ref={zipRef} onChange={onChange}></input>
+                            {errorMessage.Zip !== '' ? (<div className='Error'>{errorMessage.Zip}</div>) : null }
                         </div>
                         <div>
-                            <input type='text' placeholder='ID'ref={idRef}></input>
+                            <input type='text' name='Id' placeholder='ID' onBlur={inputValidation} ref={idRef} onChange={onChange}></input>
+                            {errorMessage.Id !== '' ? (<div className='Error'>{errorMessage.Id}</div>) : null }
                         </div>                    
                         <div className="actions">
-                            <button type='submit' onClick={newPerson} id='submit'>Submit</button>
-                            <button onClick={onClose} className="toggle-button">Close Modal</button>
+                            <button type='submit' disabled={disabled} onClick={newPerson} id='submit'>Submit</button>
+                            <button onClick={onClose} className="toggle-button">Cancel</button>
                         </div>
                     </form>
                 </div>
